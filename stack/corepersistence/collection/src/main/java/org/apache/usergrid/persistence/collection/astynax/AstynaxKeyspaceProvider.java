e482adbe9039bdae8552b466e78f67c9f9237093
Todd Nine <tnine@apigee.com>
2013-12-20 16:51:03 -0600

    Added example of host runtime update

--- stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/astynax/AstynaxKeyspaceProvider.java
+++ stack/corepersistence/collection/src/main/java/org/apache/usergrid/persistence/collection/astynax/AstynaxKeyspaceProvider.java
@@ -1,6 +1,13 @@
 package org.apache.usergrid.persistence.collection.astynax;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.usergrid.persistence.collection.cassandra.CassandraConfigEvent;
+import org.apache.usergrid.persistence.collection.cassandra.CassandraConfigListener;
+import org.apache.usergrid.persistence.collection.cassandra.ConfigChangeType;
+import org.apache.usergrid.persistence.collection.cassandra.ICassandraConfig;
 import org.apache.usergrid.persistence.collection.cassandra.IDynamicCassandraConfig;
 
 import com.google.inject.Inject;
@@ -9,6 +16,7 @@ import com.netflix.astyanax.AstyanaxConfiguration;
 import com.netflix.astyanax.AstyanaxContext;
 import com.netflix.astyanax.Keyspace;
 import com.netflix.astyanax.connectionpool.ConnectionPoolConfiguration;
+import com.netflix.astyanax.connectionpool.Host;
 import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
 import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
 import com.netflix.astyanax.connectionpool.impl.Slf4jConnectionPoolMonitorImpl;
@@ -34,20 +42,20 @@ public class AstynaxKeyspaceProvider implements Provider<Keyspace> {
 
     @Override
     public Keyspace get() {
-        AstyanaxConfiguration config = new AstyanaxConfigurationImpl()
-                .setDiscoveryType( NodeDiscoveryType.TOKEN_AWARE )
-                .setTargetCassandraVersion( cassandraConfig.getVersion() );
+        AstyanaxConfiguration config = new AstyanaxConfigurationImpl().setDiscoveryType( NodeDiscoveryType.TOKEN_AWARE )
+                                                                      .setTargetCassandraVersion(
+                                                                              cassandraConfig.getVersion() );
 
         ConnectionPoolConfiguration connectionPoolConfiguration =
-                new ConnectionPoolConfigurationImpl( "UsergridConnectionPool" )
-                        .setPort( cassandraConfig.getPort() )
-                        .setMaxConnsPerHost( cassandraConfig.getConnections() )
-                        .setSeeds( cassandraConfig.getHosts() )
-                        .setSocketTimeout( cassandraConfig.getTimeout() );
+                new ConnectionPoolConfigurationImpl( "UsergridConnectionPool" ).setPort( cassandraConfig.getPort() )
+                                                                               .setMaxConnsPerHost( cassandraConfig
+                                                                                       .getConnections() )
+                                                                               .setSeeds( cassandraConfig.getHosts() )
+                                                                               .setSocketTimeout(
+                                                                                       cassandraConfig.getTimeout() );
 
-        AstyanaxContext<Keyspace> context =
-                new AstyanaxContext.Builder().forCluster( cassandraConfig.getClusterName() )
-                        .forKeyspace( cassandraConfig.getKeyspaceName() )
+        AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder().forCluster( cassandraConfig.getClusterName() )
+                .forKeyspace( cassandraConfig.getKeyspaceName() )
 
                         /*
                          * TODO tnine Filter this by adding a host supplier.  We will get token discovery from cassandra
@@ -57,14 +65,83 @@ public class AstynaxKeyspaceProvider implements Provider<Keyspace> {
                          * load to them during runtime
                          */
 
-                        .withAstyanaxConfiguration( config )
-                        .withConnectionPoolConfiguration( connectionPoolConfiguration )
-                        .withConnectionPoolMonitor( new Slf4jConnectionPoolMonitorImpl() )
-                        .buildKeyspace( ThriftFamilyFactory.getInstance() );
+                .withAstyanaxConfiguration( config ).withConnectionPoolConfiguration( connectionPoolConfiguration )
+                .withConnectionPoolMonitor( new Slf4jConnectionPoolMonitorImpl() )
+                .buildKeyspace( ThriftFamilyFactory.getInstance() );
 
         context.start();
 
 
+        cassandraConfig.register( new ContextUpdater( context ) );
+
+
         return context.getClient();
+    }
+
+
+    private static class ContextUpdater implements CassandraConfigListener {
+
+        private final AstyanaxContext<Keyspace> context;
+
+
+        private ContextUpdater( final AstyanaxContext<Keyspace> context ) {this.context = context;}
+
+
+        @Override
+        public void reconfigurationEvent( final CassandraConfigEvent event ) {
+            ICassandraConfig config = event.getCurrent();
+
+            //aok: Do we need the event type, or should we just do a diff all the time?
+
+            //hosts have changed.
+            if(event.hasChange( ConfigChangeType.HOSTS ) || event.hasChange( ConfigChangeType.PORT )){
+                  updateHosts( event );
+            }
+        }
+
+
+        /**
+         * Update the hosts internally
+         * @param event
+         */
+        private void updateHosts(CassandraConfigEvent event){
+
+            ICassandraConfig newConfig = event.getCurrent();
+            ICassandraConfig oldConfig = event.getOld();
+
+            Set<String> oldHosts = getHosts( event.getOld().getHosts() );
+            Set<String> newHosts = getHosts( event.getCurrent().getHosts() );
+
+
+            //add all hosts, even if the exist.  Existing hosts are no-op'ed
+            for(String newHost: newHosts){
+                context.getConnectionPool().addHost( new Host(newHost, newConfig.getPort()), true );
+            }
+
+            oldHosts.removeAll( newHosts );
+
+            //if there's anything left in old hosts, remove them
+            for(String oldHost: oldHosts){
+                context.getConnectionPool().removeHost( new Host(oldHost,oldConfig.getPort() ), true );
+            }
+
+
+        }
+
+
+        /**
+         * Get all hosts as a set for intersection help
+         * @param hostString
+         * @return
+         */
+        private Set<String> getHosts(String hostString){
+            Set<String> hostSet = new HashSet<String>();
+
+            for(String string: hostString.split( "," )){
+                hostSet.add( string.trim() );
+            }
+
+            return hostSet;
+        }
     }
 }
